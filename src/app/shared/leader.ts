8415bec0a6a69fe80f9429a099fab817a1846a18
cpisgia <g.pisapia@nexsoft.it>
2017-12-01 17:05:45 +0100

    Template Forms Part 1

--- src/app/shared/leader.ts
+++ src/app/shared/leader.ts
@@ -1,9 +1,9 @@
 export class Leader {
     id: number;
     name: string;
-    image: string,
-    designation: string,
-    abbr: string,
-    featured: boolean,
+    image: string;
+    designation: string;
+    abbr: string;
+    featured: boolean;
     description: string;
 }

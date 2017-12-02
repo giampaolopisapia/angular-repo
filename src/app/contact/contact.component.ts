7664c05b568ef3eba1b7d20ede81e99f73a9bb2d
Giampaolo Pisapia <giampaolo.pisapia@gmail.com>
2017-12-02 18:22:26 +0100

    amend previous commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Feedback, ContactType } from '../shared/feedback.ts';
+import { Feedback, ContactType } from '../shared/feedback';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',

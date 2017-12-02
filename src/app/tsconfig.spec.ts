426b7854723e36e6a03ac723f87dd6c94c1a1a7a
Giampaolo Pisapia <giampaolo.pisapia@gmail.com>
2017-12-02 18:21:38 +0100

    Reactive Forms Part 1

--- src/app/tsconfig.spec.ts
+++ src/app/tsconfig.spec.ts
@@ -1,28 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import 'hammerjs';
-
+import { HttpModule } from '@angular/http';
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 import { AppComponent } from './app.component';
+import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
-import { LeaderService } from './services/leader.service';
-import { LoginComponent } from './login/login.component';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LEADERS } from '../shared/Leader';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,24 +26,20 @@ import { LoginComponent } from './login/login.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     AboutComponent,
-    ContactComponent,
-    LoginComponent
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [DishService,PromotionService,LeaderService],
-  entryComponents: [
-      LoginComponent
-    ],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
